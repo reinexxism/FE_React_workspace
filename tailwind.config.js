35b17cf355bea5581f8ab95589e9bfd3fd32a825
reinexxism <lucy0805@naver.com>
2024-03-21 10:32:47 +0900

    ADD : color theme 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "extreme-dark-black": "#1f1f1f",
+        "dark-black": "#000000",
+        "main-black": "#252525",
+        "light-black": "#2d2d2d",
+        "extreme-light-black": "#333333",
+        "extreme-light-light-black": "#3c3c3c",
+        "text-white": "#d5d5d5",
+        "dark-green": "#506651",
+        "main-green": "#7e987d",
+        "light-green": "#778974",
+        "pale-red": "#ac6363",
+        "pale-yellow": "#d5ce81",
+        "pale-green": "#6ca46e",
+      },
+    },
   },
   plugins: [],
 };
